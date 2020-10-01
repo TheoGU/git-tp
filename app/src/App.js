369083e54b3c37dbb3ee70de9d74c7e972b924e9
theo <dev@guinebertier.com>
2020-10-01 15:06:26 +0200

    resize and add classname :lipstick:

--- app/src/App.js
+++ app/src/App.js
@@ -6,15 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <section style={{display:'flex', width:'40vw', justifyContent:'space-around', flexWrap:'wrap'}}>
-          <div style={{height:'300px'}}>
+          <div className="">
             <img src="https://avatars1.githubusercontent.com/u/17689143?s=460&u=8b06e68750cb1e06e9e2a3d0c1c43ac3310eb37d&v=4" className="App-pics" alt="pics-avatar" />
             <p id="text-one">Thibeault</p>
           </div>
-          <div style={{ height: '300px' }}>
+          <div className="content-pics">
             <img src="https://avatars1.githubusercontent.com/u/31790786?s=460&u=a6187b84f87f6d0d1cdd2d008fe8c2f4f66fe14f&v=4" className="App-pics" alt="pics-avatar" />
             <p id="text-two">Théo</p>
           </div>
-          <div style={{ height: '300px' }}>
+          <div className="content-pics">
             <img src="https://avatars2.githubusercontent.com/u/62797676?s=460&v=4" className="App-pics" alt="pics-avatar" />
             <p id="text-three">Pierre</p>
           </div>
